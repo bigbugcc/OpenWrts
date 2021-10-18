60b8da38e41b0c99ae082f9ce71242aae87d391e
bigbug <1373396036@qq.com>
2021-10-18 23:18:31 +0800

    openwrt action

---
+++ configure.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Modify default system settings
+
+# sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
---
+++ package.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone https://github.com/rufengsuixing/luci-app-onliner  package/luci-app-onliner
+git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust package/luci-app-mentohust
+git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk package/MentoHUST-OpenWrt-ipk
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+git clone https://github.com/Lienol/openwrt-package package/openwrt-package

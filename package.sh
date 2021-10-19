3d61f3eba3d2af2e44e00cf3b85f664d1a320df0
bigbug <1373396036@qq.com>
2021-10-19 17:58:35 +0800

    add R2S

--- package.sh
+++ package.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-git clone https://github.com/rufengsuixing/luci-app-onliner  package/luci-app-onliner
-git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust package/luci-app-mentohust
-git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk package/MentoHUST-OpenWrt-ipk
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-git clone https://github.com/Lienol/openwrt-package package/openwrt-package
+git clone  https://github.com/bigbugcc/OpenwrtApp package/OpenwrtApp
+git clone  https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+git clone  https://github.com/xiaorouji/openwrt-passwall package/passwall
+git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot

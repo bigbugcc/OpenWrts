f2685cb7d300d21f9d73333de387e9b947b9e8ea
bigbug <1373396036@qq.com>
2021-10-20 20:03:34 +0800

    update config

--- configure.sh
+++ configure.sh
@@ -3,3 +3,6 @@
 
 # sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# 替换默认主题
+rm -rf package/lean/luci-theme-argon 
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon

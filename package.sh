81948c7df84e1f73e9bc614ded02e48c6e945e83
bigbugcc <1373396036@qq.com>
2022-08-14 16:51:18 +0800

    modify app path

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-git clone  https://github.com/bigbugcc/OpenwrtApp package/OpenwrtApp
-git clone  https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-git clone  https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+git clone  https://github.com/bigbugcc/OpenwrtApp package/otherapp/OpenwrtApp
+git clone  https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
+git clone  https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-app-pushbot
 # vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/otherapp/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/otherapp/luci-app-vssr

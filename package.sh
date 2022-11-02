667aa1229a2672625c41bc155f0cb74ce83497db
bigbug <1373396036@qq.com>
2022-11-02 18:27:59 +0800

    NewTheme
    theme

--- package.sh
+++ package.sh
@@ -5,3 +5,7 @@ git clone  https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-ap
 # vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/otherapp/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/otherapp/luci-app-vssr
+
+# Theme
+# luci-theme-neobird
+git clone https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird

afdee1057df0283dc46798f1601558d8d2d61a64
bigbug <1373396036@qq.com>
2022-11-27 16:44:26 +0800

    Update package.sh
    fix mentohust  compile error

--- package.sh
+++ package.sh
@@ -9,3 +9,6 @@ git clone https://github.com/jerrykuku/luci-app-vssr.git package/otherapp/luci-a
 # Theme
 # luci-theme-neobird
 git clone https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
+
+# Mentohust
+git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/otherapp/mentohust

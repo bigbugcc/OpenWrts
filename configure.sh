d9b2c98dc901598d9d990d6d51b7fd7a611036cb
Alvin P. Gallegos <lyh9420@icloud.com>
2023-11-28 22:11:31 +0800

    Update configure.sh
    配置ip

--- configure.sh
+++ configure.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # Modify default system settings
 
-# 修改默认IP为192.168.10.1
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
-
-# Hello World
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# 修改默认IP为192.168.18.252
+sed -i 's/192.168.18.1/192.168.18.252/g' package/base-files/files/bin/config_generate 
 
 # luci-theme-infinityfreedom
 echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default

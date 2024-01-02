f62ee0605d285558c116c8c9845c45eaababe738
bigbug <45546643+bigbugcc@users.noreply.github.com>
2024-01-02 19:21:07 +0800

    Remove vssr

--- package.sh
+++ package.sh
@@ -2,9 +2,6 @@
 git clone  https://github.com/bigbugcc/OpenwrtApp package/otherapp/OpenwrtApp
 git clone  https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
 git clone  https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-app-pushbot
-# vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/otherapp/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/otherapp/luci-app-vssr
 
 # Theme
 # luci-theme-neobird

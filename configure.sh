99a634ed93717d6c8d2490d26f4ae632c46d6f54
Ethan Bennett <48628500+yelc66@users.noreply.github.com>
2024-01-30 17:21:30 +0800

    Update configure.sh
    增加smpackage

--- configure.sh
+++ configure.sh
@@ -2,17 +2,10 @@
 # Modify default system settings
 
 # 修改默认IP为192.168.10.1
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate 
 
-# Hello World
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-
-# luci-theme-infinityfreedom
-echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
-
-# passwall
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
-echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
+# 自定义smpackage
+echo 'src-git smpackage  https://github.com/kenzok8/small-package' >>feeds.conf.default
 
 # 替换默认主题
 rm -rf package/lean/luci-theme-argon 

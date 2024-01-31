3338bb957eda67114f39c24bcbc88471f6700bd5
Ethan Bennett <48628500+yelc66@users.noreply.github.com>
2024-01-31 09:38:51 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,6 +1,1 @@
-# Theme
-# luci-theme-neobird
-git clone https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
 
-# Mentohust
-git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/otherapp/mentohust

114adddb651c6a149f3aa51839caaa6be8ce35e2
Ethan Bennett <48628500+yelc66@users.noreply.github.com>
2024-01-31 09:21:57 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -7,6 +7,4 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # 自定义smpackage
 echo 'src-git smpackage  https://github.com/kenzok8/small-package' >>feeds.conf.default
 
-# 替换默认主题
-rm -rf package/lean/luci-theme-argon 
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
+

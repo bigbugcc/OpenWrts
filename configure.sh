aae9bc318b6175872574807185629a6975279b2c
Ethan Bennett <48628500+yelc66@users.noreply.github.com>
2024-02-01 14:45:45 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,7 +2,10 @@
 # Modify default system settings
 
 # 修改默认IP为192.168.10.1
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate 
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# luci-theme-infinityfreedom
+echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
 
 # 自定义smpackage
 echo 'src-git smpackage  https://github.com/kenzok8/small-package' >>feeds.conf.default

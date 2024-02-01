f1dc6daf0e390d7fb1e507a1c5f2c34c28704ba9
Ethan Bennett <48628500+yelc66@users.noreply.github.com>
2024-02-01 14:44:40 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+git clone  https://github.com/bigbugcc/OpenwrtApp package/otherapp/OpenwrtApp
+git clone  https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
+git clone  https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-app-pushbot
 
+# Theme
+# luci-theme-neobird
+git clone https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
+
+# Mentohust
+git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/otherapp/mentohust
+
+# UnblockNeteaseMusic
+git clone -b master  https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/unblockneteasemusic

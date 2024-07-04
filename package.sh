91f4872a521c7d72773b5fd7da526ad1946e8de8
bigbug <1373396036@qq.com>
2024-07-04 20:38:30 +0800

    Reduce clone repo size

--- package.sh
+++ package.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-git clone  https://github.com/bigbugcc/OpenwrtApp package/otherapp/OpenwrtApp
-git clone  https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
-git clone  https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-app-pushbot
+git clone --depth 1 https://github.com/bigbugcc/OpenwrtApp package/otherapp/OpenwrtApp
+git clone --depth 1 https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
+git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-app-pushbot
 
 # Theme
 # luci-theme-neobird
-git clone https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
+git clone --depth 1 https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
 
 # Mentohust
-git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/otherapp/mentohust
+git clone --depth 1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/otherapp/mentohust
 
 # UnblockNeteaseMusic
-git clone -b master  https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/unblockneteasemusic
+git clone --depth 1 -b master  https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/unblockneteasemusic
 
 # OpenClash
-git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+git clone --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash

dc5e61b4c3f8cb20092530b01b0127f71afa6b13
bigbug <ibigbugcc@gmail.com>
2024-10-24 12:44:18 +0800

    Luci rollback

--- configure.sh
+++ configure.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Modify default system settings
 
+# 暂时回滚
+sed -i '/^#src-git luci https:\/\/github.com\/coolsnowwolf\/luci$/s/^#//' feeds.conf.default && sed -i '/^src-git luci https:\/\/github.com\/coolsnowwolf\/luci\.git;openwrt-23\.05$/s/^/#/' feeds.conf.default
+
 # 修改默认IP为192.168.10.1
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
 

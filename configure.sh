2d334a8c3a6be9621496dec1774bd1864ce88b7b
dxs1256 <23136671+dxs1256@users.noreply.github.com>
2024-11-14 21:10:21 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,9 +4,6 @@
 # 暂时回滚
 sed -i '/^#src-git luci https:\/\/github.com\/coolsnowwolf\/luci$/s/^#//' feeds.conf.default && sed -i '/^src-git luci https:\/\/github.com\/coolsnowwolf\/luci\.git;openwrt-23\.05$/s/^/#/' feeds.conf.default
 
-# luci-theme-infinityfreedom
-echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
-
 # 替换默认主题
 rm -rf package/lean/luci-theme-argon 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon

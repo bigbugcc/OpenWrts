5ed7cca1e036261b80d5eaf1e37a720b93969910
bigbug <ibigbugcc@gmail.com>
2025-10-09 13:00:32 +0000

    Remove oldpackage, add default setting

--- package.sh
+++ package.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-git clone --depth 1 https://github.com/bigbugcc/OpenwrtApp package/otherapp/OpenwrtApp
+git clone --depth 1 https://github.com/kenzok8/small-package package/otherapp/small-package
 git clone --depth 1 https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
-git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-app-pushbot
 
 # Theme
 # luci-theme-neobird
@@ -9,9 +8,6 @@ git clone --depth 1 https://github.com/thinktip/luci-theme-neobird.git package/o
 
 # Mentohust
 git clone --depth 1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/otherapp/mentohust
-
-# UnblockNeteaseMusic
-git clone --depth 1 -b master  https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/unblockneteasemusic
 
 # OpenClash
 git clone --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash

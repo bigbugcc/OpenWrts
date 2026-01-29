986ddafbecb9b4af239b6e448a23e47532e91f99
bigbug <ibigbugcc@gmail.com>
2026-01-29 01:39:04 +0000

    Remove unsupported components

--- package.sh
+++ package.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-git clone --depth 1 https://github.com/bigbugcc/OpenwrtApp package/otherapp/OpenwrtApp
 git clone --depth 1 https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
 git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-app-pushbot
-
-# Theme
-# luci-theme-neobird
-git clone --depth 1 https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
 
 # Mentohust
 git clone --depth 1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/otherapp/mentohust

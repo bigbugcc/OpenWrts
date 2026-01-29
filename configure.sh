a64dea1b50b863f8ce8f6daa8aff77b7e67f878d
bigbug <ibigbugcc@gmail.com>
2026-01-29 01:15:34 +0000

    passwall2

--- configure.sh
+++ configure.sh
@@ -8,8 +8,8 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # passwall
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
-echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
+echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" >> feeds.conf.default
+echo "src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main" >> feeds.conf.default
 
 # iStore
 echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default

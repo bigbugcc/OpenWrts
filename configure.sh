3e11625632b5c1e0ab9e281e4ea052c756dad3e7
timptbug <57766076+timptbug@users.noreply.github.com>
2026-02-10 16:18:55 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Modify default system settings
 
-# 修改默认IP为192.168.10.1
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
+# 修改默认IP为192.168.2.1
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate 
 
 # Hello World
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
